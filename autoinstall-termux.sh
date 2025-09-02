664785c9272ac2496bfdcdd123edc7084a0af5b2
Victor <vibarrientosp@gmail.com>
2025-09-02 00:00:27 -0500

    Add autoinstall file for termux environment
    This is meant to be used with the shell syntax 'curl <file URL> | sh' to install dotfiles with just one command.

---
+++ autoinstall-termux.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -
+
+$curdir=$(pwd)
+
+cd $HOME
+
+git clone --depth=1 https://github.com/barrientosvctor/dotfiles.git $HOME/.dotfiles
+
+chmod +x $HOME/.dotfiles/bootstrap-termux.sh
+$HOME/.dotfiles/bootstrap-termux.sh
+
+cd $curdir
