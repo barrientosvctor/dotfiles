b472f649c26878914c12f3e143c6f236afa35e1f
Victor <vibarrientosp@gmail.com>
2025-09-01 23:58:55 -0500

    Add bootstrap file for termux environment

---
+++ bootstrap-termux.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -
+
+termux-setup-storage
+
+pkg --check-mirror update
+pkg update && pkg upgrade
+
+pkg install vim tmux git wget fzf nodejs-lts
+
+ln --force -s $HOME/.dotfiles/.config/vim/.vimrc $HOME/.vimrc
+ln --force -s $HOME/.dotfiles/.config/tmux/.tmux.conf $HOME/.tmux.conf
+
+# Install Vim-Plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim -es -u $HOME/.vimrc -i NONE -c "PlugInstall" -c "qa"
